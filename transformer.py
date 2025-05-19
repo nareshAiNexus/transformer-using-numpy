9f75cca7187bf8287e72ff74f6f1250d060aa5f2
NareshAiNexus <nare95143@gmail.com>
2025-05-19 07:23:18 +0530

    created class object for encoder and decoder

--- transformer.py
+++ transformer.py
@@ -1,4 +1,12 @@
 # Creating a transformer from scratch
 # Reference : "All you needed is this" research paper
 
+import numpy as np 
+
+# creating neural network of 6 layers
+
+class Transformer:
+    def __init__(self):
+        pass
+
 
