6544720bcb4e2a2d2ef2568a29642fa16e1971ed
NareshAiNexus <nare95143@gmail.com>
2025-05-20 00:19:46 +0530

    transformer using pytorch for reference created

---
+++ transformer_pytorch.py
@@ -0,0 +1,36 @@
+# Import pytorch and all the libraries
+
+import torch 
+import torch.nn as nn
+import torch.optim as optim
+import math 
+import copy
+
+class MultiheadAttention(nn.model):
+    def __init__(self, d_model, num_heads):
+        super(MultiheadAttention, self).__init__()
+        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
+
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_k = d_model // num_heads
+
+        self.W_q = nn.Linear(d_model, d_model)
+        self.W_k = nn.Linear(d_model, d_model)
+        self.W_v = nn.Linear(d_model, d_model)
+        self.W_o = nn.Linear(d_model, d_model)
+
+    def scaled_dot_product_attention(self, Q, K, V, mask=None):
+        attn_scores = torch.matmul(Q, K.transpose(-2, -1)) / math.swrt(self.d_k)
+        if mask is not None:
+            attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
+        attn_probs = torch.softmax(attn_scores, dim=-1)
+        output = torch.matmul(attn_probs, V)
+        return output
+
+    def split_heads(self, x):
+        batch_size, seq_length, d_model = x.size()
+        return x.view(batch_size, seq_length, self.num_heads, self.d_k).transpose(1, 2)
+
+
+
