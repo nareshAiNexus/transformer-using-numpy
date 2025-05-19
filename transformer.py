c75909f061b8693ce98240efbf0915695d7a9a28
NareshAiNexus <nare95143@gmail.com>
2025-05-19 07:47:41 +0530

    transformer.py updated

--- transformer.py
+++ transformer.py
@@ -3,14 +3,23 @@
 
 import numpy as np 
 
-# creating neural network of 6 layers
+# creating neural network of 2(6) layers
 
 class Transformer:
     def __init__(self):
         pass
 
-    def forward_pass(self):
+# The encoder is composed of a stack of N = 6 identical layers.
+# Each layer has 2 sub-layers .
+# The first is a multi head self-attention mechanism.
+# The second is a simple, position-wise fully connected feed forward layer
+
+class Encoder:
+    def __init__(self):
         pass
 
-    def encoder(self):
-        pass 
+class Decoder:
+    def __init__(self):
+        pass
+
+
